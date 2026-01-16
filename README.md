de02d62f8c622752284433eec613f6b69724d6ff
John Swift <johnswift@live.com>
2026-01-16 02:49:34 -0500

    Init

---
+++ README.md
@@ -0,0 +1,3 @@
+Recently I had the pleasure of jail-breaking a very old iPad to recover some things off of it. One of the most annoying things I experienced was checkrain's only tool for getting a terminal to PongoOS only having a makefile for MacOS.
+
+This is a python script that follows the same protocol as shown in pongoterm.c
---
+++ main.py
@@ -0,0 +1,55 @@
+import usb.core
+import usb.util
+import sys
+
+dev = usb.core.find(idVendor=0x05AC, idProduct=0x4141)
+if not dev:
+    print("PongoOS not found")
+    sys.exit(1)
+
+try:
+    if dev.is_kernel_driver_active(0):
+        dev.detach_kernel_driver(0)
+except:
+    pass
+
+dev.set_configuration()
+usb.util.claim_interface(dev, 0)
+
+
+def pongo_read():
+    output = b""
+    while True:
+        status = dev.ctrl_transfer(0xA1, 2, 0, 0, 1)
+        try:
+            data = dev.ctrl_transfer(0xA1, 1, 0, 0, 0x1000)
+            if data:
+                output += bytes(data)
+                print(bytes(data).decode(), end="", flush=True)
+        except:
+            pass
+        if status[0] == 0:
+            break
+    return output
+
+
+def pongo_send(cmd):
+    if isinstance(cmd, str):
+        cmd = cmd.encode()
+    if not cmd.endswith(b"\n"):
+        cmd += b"\n"
+    dev.ctrl_transfer(0x21, 4, 1, 0, None)
+    dev.ctrl_transfer(0x21, 3, 0, 0, cmd)
+
+
+pongo_read()
+
+while True:
+    try:
+        cmd = input()
+        pongo_send(cmd)
+        pongo_read()
+    except EOFError:
+        break
+    except KeyboardInterrupt:
+        break
